f112f900467d7850174010d46fbeb8f93e2bbfc7
Hunter <273676636@qq.com>
2022-04-27 21:13:45 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './styles'
+// import './mock'
+
+const app = createApp(App)
+
+app.use(router)
+  .use(store)
+  .mount('#app')
